7d683f2b53c5beaad548b785f9de46d0e29f00c5
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-08 08:31:34 +0900

    Update GUI.js

--- javasciprt/GUI.js
+++ javasciprt/GUI.js
@@ -86,7 +86,28 @@ bardButton.style.fontSize = "14px";
 document.body.appendChild(bardButton);
 
 bardButton.addEventListener("click", function() {
-javascript:(function(){var userQuestion=prompt("質問を入力してください:");if(!userQuestion.trim()){alert("質問が空白です。");return;}var gasScriptUrl="https://script.google.com/macros/s/AKfycbwr_BSlLobuQkMKsAMDLTFlYCbPs_hGv7gO3LZrwcBCCvNu1K3VVwqhQ_S56T10N_3j/exec";gasScriptUrl+="?question="+encodeURIComponent(userQuestion);fetch(gasScriptUrl).then(response=>{if(!response.ok){throw new Error(%27Network response was not ok%27);}return response.json();}).then(data=>{alert(data.text);}).catch(error=>{console.error(%27There has been a problem with your fetch operation:%27,error);});})();
+(function () {
+  var userQuestion = prompt("質問を入力してください:");
+  if (!userQuestion.trim()) {
+    alert("質問が空白です。");
+    return;
+  }
+  var gasScriptUrl = "https://script.google.com/macros/s/AKfycbwr_BSlLobuQkMKsAMDLTFlYCbPs_hGv7gO3LZrwcBCCvNu1K3VVwqhQ_S56T10N_3j/exec";
+  gasScriptUrl += "?question=" + encodeURIComponent(userQuestion);
+  fetch(gasScriptUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      alert(data.text);
+    })
+    .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
+})();
     // ボタン非表示
     bardButton.style.display = "none";
 });
