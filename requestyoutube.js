e9215358f67f6e8cd410cda427cc450e03119d7f
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-14 19:26:09 +0900

    Update requestyoutube.js

--- requestyoutube.js
+++ requestyoutube.js
@@ -1,23 +1,31 @@
-// Google Apps ScriptのURL
 var scriptUrl = 'https://script.google.com/macros/s/AKfycbyDO4z3Msz0NRuJb8lolw8X71fCdcuhUH0O9dihMvYdlZyU0IRILNwAdOSKJS7a0Ca-/exec';
 
-// 検索クエリと取得する動画数を指定
-var query = prompt("ここに検索したいyoutubeのキーワードを入れてください。");
-var maxResults = 5;
+var query;
+do {
+  query = prompt("ここに検索したいYouTubeのキーワードを入れてください。");
+  if (query.trim() === "") {
+    alert("検索キーワードが空白です。再入力してください。");
+  }
+} while (query.trim() === "");
 
-// Google Apps ScriptのdoGet関数にリクエストを送信 (GETリクエスト)
+var maxResults;
+do {
+  maxResults = prompt("表示したい動画の数を入力してください（最大30）");
+  if (maxResults >= 20) {
+    alert("注意: 動画の数が20以上になると、取得に時間がかかる可能性があります。");
+  }
+} while (!/^\d+$/.test(maxResults) || maxResults < 1 || maxResults > 30);
+
 var url = scriptUrl + '?q=' + encodeURIComponent(query) + '&a=' + encodeURIComponent(maxResults);
 fetch(url, {
   method: 'GET'
 })
-  .then(response => response.json()) // JSON形式で返ってくることを想定
+  .then(response => response.json())
   .then(data => {
-    // HTMLを構築して表示する
     var html = '<ul>';
     data.videos.forEach(video => {
       html += '<li>';
       html += '<h3>' + video.title + '</h3>';
-      // YouTubeの埋め込みコードがあれば追加
       if (video.iframe) {
         html += video.iframe;
       }
@@ -25,7 +33,6 @@ fetch(url, {
     });
     html += '</ul>';
 
-    // HTMLをドキュメントに追加
     document.body.innerHTML = html;
   })
   .catch(error => console.error('Error:', error));
