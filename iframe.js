c2e6abdfb6ab66aa6505f5b9d246347d0b6f7971
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-16 16:47:28 +0900

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,11 +1,30 @@
-  // 新しい iframe を作成
-  var iframe = document.createElement('iframe');
+// service-worker.js
 
-  // iframe の属性を設定
-  iframe.src = 'https://google.com?igu=1'; // iframe のURLを指定
-  iframe.width = '600'; // iframe の幅を指定
-  iframe.height = '400'; // iframe の高さを指定
-  iframe.frameBorder = '0'; // 枠線を非表示にする
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+});
 
-  document.body.appendChild(iframe);
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+});
 
+self.addEventListener('fetch', event => {
+  // Handle fetch events if needed
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.action === 'createIframe') {
+    // 新しい iframe を作成
+    var iframe = new DOMParser().parseFromString(
+      '<iframe src="https://google.com?igu=1" width="600" height="400" frameborder="0"></iframe>',
+      'text/html'
+    ).body.firstChild;
+
+    // 作成した iframe をブラウザに表示
+    clients.matchAll().then(clients => {
+      clients.forEach(client => {
+        client.postMessage({ action: 'showIframe', iframeHTML: iframe.outerHTML });
+      });
+    });
+  }
+});
