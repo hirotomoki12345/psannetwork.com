38a8b584bf797a61e411357c8fc3b786cc8f378f
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-16 16:48:45 +0900

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -15,15 +15,12 @@ self.addEventListener('fetch', event => {
 self.addEventListener('message', event => {
   if (event.data && event.data.action === 'createIframe') {
     // 新しい iframe を作成
-    var iframe = new DOMParser().parseFromString(
-      '<iframe src="https://google.com?igu=1" width="600" height="400" frameborder="0"></iframe>',
-      'text/html'
-    ).body.firstChild;
+    var iframeHTML = '<iframe src="https://google.com?igu=1" width="600" height="400" frameborder="0"></iframe>';
 
     // 作成した iframe をブラウザに表示
     clients.matchAll().then(clients => {
       clients.forEach(client => {
-        client.postMessage({ action: 'showIframe', iframeHTML: iframe.outerHTML });
+        client.postMessage({ action: 'showIframe', iframeHTML: iframeHTML });
       });
     });
   }
