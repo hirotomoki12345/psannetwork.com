5f182b8487c189f93e5b23877120a78b6860ef11
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-21 11:15:49 +0900

    Create sw.js

---
+++ javasciprt/sw.js
@@ -0,0 +1,24 @@
+// sw.js
+
+self.addEventListener('install', (event) => {
+  console.log('Service Workerがインストールされました');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Workerがアクティブになりました');
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log('リクエストがフェッチされました: ', event.request.url);
+});
+
+self.addEventListener('keydown', (event) => {
+  if (event.altKey && event.keyCode === 79) {
+    // Alt + O が押されたらメッセージをブラウザに送信
+    self.clients.matchAll().then((clients) => {
+      clients.forEach((client) => {
+        client.postMessage('Alt + O が押されました！');
+      });
+    });
+  }
+});
