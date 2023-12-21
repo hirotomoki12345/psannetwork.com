2ed31383883e8cfbdc5e79006e44fe95a0e67505
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-22 07:30:11 +0900

    Update crash2.1.js

--- javasciprt/拡張機能停止js/crash2.1.js
+++ javasciprt/拡張機能停止js/crash2.1.js
@@ -3,7 +3,6 @@ setTimeout(function() {
     let img = new Image();
     img.src = 'http://evilwebsite.com/large.jpg';
     img.onload = function() {
-      // 画像の読み込みに時間がかかる処理を追加する
       for (let i = 0; i < 10000; i++) {
         Math.pow(Math.PI, Math.random());
       }
@@ -11,3 +10,4 @@ setTimeout(function() {
     document.body.appendChild(img);
   }
 }, 2000);
+//©Psan network
