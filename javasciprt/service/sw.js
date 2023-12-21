47fe76c5bd1377ee09c96ce7cefe263537c203ed
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-21 11:20:36 +0900

    Update sw.js

--- javasciprt/service/sw.js
+++ javasciprt/service/sw.js
@@ -1,24 +1,34 @@
-// sw.js
-
-self.addEventListener('install', (event) => {
-  console.log('Service Workerがインストールされました');
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('Service Workerがアクティブになりました');
-});
-
-self.addEventListener('fetch', (event) => {
-  console.log('リクエストがフェッチされました: ', event.request.url);
-});
-
-self.addEventListener('keydown', (event) => {
+// Alt + O キーが押されたときの処理を追加
+window.addEventListener('keydown', (event) => {
   if (event.altKey && event.keyCode === 79) {
-    // Alt + O が押されたらメッセージをブラウザに送信
-    self.clients.matchAll().then((clients) => {
-      clients.forEach((client) => {
-        client.postMessage('Alt + O が押されました！');
-      });
-    });
+    executeScript();
   }
 });
+
+function executeScript() {
+  try {
+    var code = document.getElementById('customScript').value;
+    eval(code);
+  } catch (error) {
+    alert("エラー: " + error.message);
+  }
+}
+
+(function () {
+  var body = document.body;
+
+  var style = document.createElement("style");
+  style.textContent = 'body,html{height:100%;margin:0;}input[type="text"]{width:100%;height:100%;font-size:18px;padding:10px;box-sizing:border-box;}button{display:block;margin:10px auto;font-size:18px;}';
+  body.appendChild(style);
+
+  var input = document.createElement("input");
+  input.type = "text";
+  input.id = "customScript"; // 追加：入力フィールドにIDを設定
+  input.placeholder = "JavaScriptコードを入力してください";
+  body.appendChild(input);
+
+  var button = document.createElement("button");
+  button.textContent = "実行";
+  button.addEventListener("click", executeScript); // 変更：実行ボタンがクリックされた時に関数を呼ぶ
+  body.appendChild(button);
+})();
