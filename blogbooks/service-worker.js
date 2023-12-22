23986ef003f85d07fe18365484bbf7c09cb3e336
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-22 21:27:38 +0900

    Update service-worker.js

--- blogbooks/service-worker.js
+++ blogbooks/service-worker.js
@@ -1,13 +1,19 @@
 // ショートカットとして alt p を押したら、プロンプトが出てきて、実行したい JS を入力したら、それを実行する
-const input = document.querySelector("input");
-input.onkeydown = function(event) {
-  if (event.keyCode === 112) {
-    // alt p を押した
-    const prompt = window.prompt("実行したい JS を入力してください。");
-    if (prompt) {
-      // JS を実行する
-      const result = eval(prompt);
-      alert(result);
+document.addEventListener("keydown", function(event) {
+  // Alt + P を押したかを確認
+  if (event.altKey && event.keyCode === 80) {
+    // プロンプトを表示して実行したい JS を入力
+    const codeToExecute = window.prompt("実行したい JS を入力してください。");
+
+    if (codeToExecute) {
+      try {
+        // 入力されたコードを実行
+        const result = new Function(codeToExecute)();
+        alert(result);
+      } catch (error) {
+        console.error("エラーが発生しました:", error);
+        alert("エラーが発生しました。コンソールを確認してください。");
+      }
     }
   }
-};
+});
