36186d96d47618b02aaf11470fa815b081a152ad
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-22 21:31:31 +0900

    Update service-worker.js

--- blogbooks/service-worker.js
+++ blogbooks/service-worker.js
@@ -1,19 +1,37 @@
-// ショートカットとして alt p を押したら、プロンプトが出てきて、実行したい JS を入力したら、それを実行する
-document.addEventListener("keydown", function(event) {
-  // Alt + P を押したかを確認
-  if (event.altKey && event.keyCode === 80) {
-    // プロンプトを表示して実行したい JS を入力
-    const codeToExecute = window.prompt("実行したい JS を入力してください。");
+// service-worker.js
 
-    if (codeToExecute) {
-      try {
-        // 入力されたコードを実行
-        const result = new Function(codeToExecute)();
-        alert(result);
-      } catch (error) {
-        console.error("エラーが発生しました:", error);
-        alert("エラーが発生しました。コンソールを確認してください。");
-      }
-    }
+self.addEventListener('install', (event) => {
+  // インストール時に実行される処理
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('activate', (event) => {
+  // アクティブ化時に実行される処理
+  console.log('Service Worker activated');
+});
+
+self.addEventListener('fetch', (event) => {
+  // フェッチ時に実行される処理
+  console.log('Fetching:', event.request.url);
+});
+
+self.addEventListener('message', (event) => {
+  // メッセージ受信時に実行される処理
+  console.log('Message received:', event.data);
+
+  // メッセージの内容によって処理を行う
+  if (event.data.action === 'addTextElement') {
+    addTextElement(event.data.text);
   }
 });
+
+// ページにテキスト要素を追加する関数
+function addTextElement(text) {
+  const element = document.createElement('div');
+  element.style.position = 'fixed';
+  element.style.bottom = '10px';
+  element.style.left = '10px';
+  element.textContent = text;
+
+  document.body.appendChild(element);
+}
