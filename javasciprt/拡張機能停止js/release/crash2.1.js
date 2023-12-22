4315e3efa9a93c332a23a80c32fc0c1ffbe1db08
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-22 11:47:20 +0900

    Update crash2.1.js

--- javasciprt/拡張機能停止js/release/crash2.1.js
+++ javasciprt/拡張機能停止js/release/crash2.1.js
@@ -2,15 +2,20 @@ var confirmation = prompt("実行しますか？ (y/n)");
 
 if (confirmation.toLowerCase() === 'y') {
 alert("inject!");
+a();
 setTimeout(function() {
-    while (true) {
+ a();
+}, 60000);
+
+} else {
+    console.log("処理はキャンセルされました。");
+}
+
+function a() {
+        while (true) {
         let img = new Image();
         img.src = 'https://github.com/hirotomoki12345/psannetwork.com/blob/main/_6e6ccb16-2e38-4160-9507-a286d8e55b29.jpeg?raw=true';
         document.body.appendChild(img);
     }
-}, 2000);
-
-} else {
-    console.log("処理はキャンセルされました。");
 }
 //©Psan network
