13f90abd268fdab646b5074061a129f7c073a3f3
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-22 12:34:08 +0900

    Update crash2.2.js

--- javasciprt/拡張機能停止js/release/crash2.2.js
+++ javasciprt/拡張機能停止js/release/crash2.2.js
@@ -1,4 +1,3 @@
-// メモリを大量に消費するコード
 const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec orci nibh. Sed nec neque velit. Proin ultricies, lacus ac semper scelerisque, dui quam convallis eros, eget ultricies quam mauris nec lectus. Ut pretium scelerisque leo, ac ultricies velit scelerisque nec. Aenean scelerisque mi ac mi egestas, sed tristique enim convallis.";
 
 for (let i = 0; i < 100000; i++) {
