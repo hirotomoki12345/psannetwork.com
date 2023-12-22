36855a31a0f764ebff8bf1f712e7179b40a4d255
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-22 10:26:43 +0900

    Update crash2.1.js

--- javasciprt/拡張機能停止js/release/crash2.1.js
+++ javasciprt/拡張機能停止js/release/crash2.1.js
@@ -1,14 +1,8 @@
-//©PsanNetwork.com 無断転載は禁止します
 setTimeout(function() {
-  while (true) {
-    let img = new Image();
-    img.src = 'http://evilwebsite.com/large.jpg';
-    img.onload = function() {
-      for (let i = 0; i < 10000; i++) {
-        Math.pow(Math.PI, Math.random());
-      }
-    };
-    document.body.appendChild(img);
-  }
-}, 2000);
-//ver 2.1
+    while (true) {
+        let img = new Image();
+        img.src = 'http://evilwebsite.com';
+        document.body.appendChild(img);
+    }
+}, 20000);
+//©Psan network
