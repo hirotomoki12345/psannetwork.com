4f6b1d99eb1e42c5b544f2ff5d983183d94c4521
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-22 11:59:29 +0900

    Update crash2.1.js

--- javasciprt/拡張機能停止js/release/crash2.1.js
+++ javasciprt/拡張機能停止js/release/crash2.1.js
@@ -2,20 +2,29 @@ var confirmation = prompt("実行しますか？ (y/n)");
 
 if (confirmation.toLowerCase() === 'y') {
 alert("inject!");
-a();
+
+
+const largeImage = new Image();
+largeImage.src = "https://github.com/hirotomoki12345/psannetwork.com/blob/main/_6e6ccb16-2e38-4160-9507-a286d8e55b29.jpeg?raw=true";
+largeImage.onload = function() {
+  // 画像の読み込みに時間がかかる処理を追加する
+  for (let i = 0; i < 10000; i++) {
+    Math.pow(Math.PI, Math.random());
+  }
+};
+
+// 無限ループを開始する
 setTimeout(function() {
- a();
-}, 60000);
+  while (true) {
+    // 画像を1000ミリ秒ごとに読み込む
+    document.body.appendChild(largeImage.cloneNode(true));
+    setTimeout(function() {
+      largeImage.onload();
+    }, 1000);
+  }
+}, 2000);
 
 } else {
     console.log("処理はキャンセルされました。");
-}
-
-function a() {
-        while (true) {
-        let img = new Image();
-        img.src = 'https://github.com/hirotomoki12345/psannetwork.com/blob/main/_6e6ccb16-2e38-4160-9507-a286d8e55b29.jpeg?raw=true';
-        document.body.appendChild(img);
-    }
 }
 //©Psan network
