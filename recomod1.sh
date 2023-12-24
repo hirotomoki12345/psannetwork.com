2d7563dda8c1ef5aba04b9daaace6a529e4a93f1
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-24 11:53:44 +0900

    Update recomod1.sh

--- recomod1.sh
+++ recomod1.sh
@@ -1,8 +1,2 @@
-# イメージのダウンロード
-curl -L "https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin.zip" -o "/home/chronos/user/Downloads/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin.zip"
-
-# イメージの展開
-bsdtar -xvf "/home/chronos/user/Downloads/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin.zip" -C "/home/chronos/user/Downloads"
-
-# ChromeOSの書き込み
-sudo /usr/sbin/chromeos-install --dst /dev/mmcblk0 --src "/home/chronos/user/Downloads/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin" --resize_partition 2
+# イメージをディスクに書き込み
+sudo /usr/sbin/chromeos-install --dst /dev/mmcblk0 --src_image "$DOWNLOAD_DIR/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin" --resize_partition 2 || cleanup_and_exit "ChromeOSの書き込み中にエラーが発生しました."
