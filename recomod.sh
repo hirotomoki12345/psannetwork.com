e4d11b76646a707f96df28f6854f2566bc66f198
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-24 09:52:26 +0900

    Update recomod.sh

--- recomod.sh
+++ recomod.sh
@@ -17,44 +17,31 @@ function cleanup_and_exit {
     # エラーメッセージをファイルに保存
     echo "エラー: $1" >> "$ERROR_LOG"
     
-    # 作業ディレクトリから出る
-    cd ~
-    
     # ダウンロードしたファイルを削除
     rm -f os-image.zip os-image.bin
     
-    # ルートディレクトリをマウント
-    sudo mount -o remount,rw /
-    
-    # ルートディレクトリをアンマウント
-    sudo umount /
-
     exit $exit_code
 }
 
 # トラップを設定してエラーが発生した場合に cleanup_and_exit を呼ぶ
 trap 'cleanup_and_exit "スクリプトの実行中にエラーが発生しました."' ERR
 
-# ルートディレクトリをアンマウント
-sudo umount / || cleanup_and_exit "ルートディレクトリのアンマウント中にエラーが発生しました。"
-
-# ディスクを検索し、ルートディレクトリをフォーマット
-DISK=$(lsblk -no NAME,MOUNTPOINT | awk '$2=="/"{print $1}')
-sudo mkfs.ext4 /dev/$DISK || cleanup_and_exit "ディスクのフォーマット中にエラーが発生しました。"
-
-# 作業ディレクトリに移動
-cd ~
+# ルートディレクトリのマウントを確認
+if mount | grep " / " | grep -q "rw"; then
+    # ルートディレクトリが読み取り専用でない場合はアンマウント
+    sudo umount / || cleanup_and_exit "ルートディレクトリのアンマウント中にエラーが発生しました。"
+fi
 
 # ダウンロードしたイメージを展開
-curl -o os-image.zip https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin.zip || cleanup_and_exit "イメージのダウンロード中にエラーが発生しました。"
-unzip os-image.zip || cleanup_and_exit "イメージの展開中にエラーが発生しました。"
-rm -f os-image.zip || cleanup_and_exit "ダウンロードしたZIPファイルの削除中にエラーが発生しました。"
+curl -o os-image.zip https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin.zip || cleanup_and_exit "イメージのダウンロード中にエラーが発生しました."
+unzip os-image.zip || cleanup_and_exit "イメージの展開中にエラーが発生しました."
+rm -f os-image.zip || cleanup_and_exit "ダウンロードしたZIPファイルの削除中にエラーが発生しました."
 
 # イメージをディスクに書き込み
-sudo dd if=os-image.bin of=/dev/$DISK bs=1M || cleanup_and_exit "イメージの書き込み中にエラーが発生しました。"
+sudo dd if=os-image.bin of=/dev/mmcblk0 bs=1M || cleanup_and_exit "イメージの書き込み中にエラーが発生しました."
 
 # 書き込みが成功したかを確認
 echo "イメージの書き込みが成功しました。"
 
-# 再起動
-sudo reboot
+# Chromebookでは再起動はできないため、メッセージの表示のみ
+echo "Chromebookを再起動してください。"
