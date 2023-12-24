e3c707b896a1d39162d4890fe9b2ac09fc78d86b
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-24 21:33:12 +0900

    Update recomod1.sh

--- recomod1.sh
+++ recomod1.sh
@@ -9,20 +9,26 @@ DOWNLOAD_DIR="/home/chronos/user/Downloads"
 ERROR_DIR="$DOWNLOAD_DIR/flasherror"
 ERROR_LOG="$ERROR_DIR/error_log.txt"
 
+# バックアップ先ディレクトリ
+BACKUP_DIR="/home/chronos/user/Downloads"
+
 # エラーファイルのディレクトリが存在しなければ作成
 mkdir -p "$ERROR_DIR"
+
+# バックアップ先ディレクトリが存在しなければ作成
+mkdir -p "$BACKUP_DIR"
 
 # エラー時の処理を定義
 function cleanup_and_exit {
     local exit_code=$?
     echo "エラー: $1"
-
+    
     # エラーメッセージをファイルに保存
     echo "エラー: $1" >> "$ERROR_LOG"
-
+    
     # ダウンロードしたファイルを削除
     rm -f "$DOWNLOAD_DIR/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin.zip"
-
+    
     exit $exit_code
 }
 
@@ -32,24 +38,39 @@ trap 'cleanup_and_exit "スクリプトの実行中にエラーが発生しま�
 # ルートディレクトリのマウントを確認
 if mount | grep " / " | grep -q "rw"; then
     # ルートディレクトリが読み取り専用でない場合はアンマウント
-    sudo umount / || cleanup_and_exit "ルートディレクトリのアンマウント中にエラーが発生しました."
+    sudo umount / || cleanup_and_exit "ルートディレクトリのアンマウント中にエラーが発生しました。"
 fi
 
-# ダウンロードしたイメージが存在しない場合のみダウンロード
-if [ ! -f "$DOWNLOAD_DIR/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin" ]; then
-    # イメージをダウンロード
+# ユーザーにモードを選択させる
+read -p "モードを選択してください（1: イメージ書き込み, 2: バックアップ作成）: " mode
+
+# 選択されたモードに応じて処理を実行
+if [ "$mode" == "1" ]; then
+    # ダウンロードしたイメージをディスクに書き込み
     curl -L "https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin.zip" -o "$DOWNLOAD_DIR/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin.zip" || cleanup_and_exit "イメージのダウンロード中にエラーが発生しました."
 
     # Chromebookでは標準のunzipコマンドではなくbsdtarを使用する
     bsdtar -xvf "$DOWNLOAD_DIR/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin.zip" -C "$DOWNLOAD_DIR" || cleanup_and_exit "イメージの展開中にエラーが発生しました."
+
+    # イメージをディスクに書き込み
+    sudo dd if="$DOWNLOAD_DIR/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin" of=/dev/mmcblk0 bs=4M conv=fsync || cleanup_and_exit "イメージの書き込み中にエラーが発生しました."
+
+    # 書き込みが成功したかを確認
+    echo "イメージの書き込みが成功しました。"
+
+    # Chromebookでは再起動はできないため、メッセージの表示のみ
+    echo "Chromebookを再起動してください。"
+    reboot
+
+elif [ "$mode" == "2" ]; then
+    # バックアップ先ディレクトリにバックアップイメージを作成
+    backup_file="$BACKUP_DIR/chromeos_backup_$(date +"%Y%m%d_%H%M%S").img"
+    sudo dd if=/dev/mmcblk0 of="$backup_file" bs=4M conv=fsync || cleanup_and_exit "バックアップの作成中にエラーが発生しました."
+    
+    # バックアップが成功したかを確認
+    echo "バックアップの作成が成功しました。"
+    
+else
+    # 無効なモードが選択された場合のエラー処理
+    cleanup_and_exit "無効なモードが選択されました。"
 fi
-
-# イメージをディスクに書き込み (ddコマンドを使用)
-sudo dd if="$DOWNLOAD_DIR/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin" of=/dev/mmcblk0 bs=4M status=progress
-
-# 書き込みが成功したかを確認
-echo "ChromeOSの書き込みが成功しました。"
-
-# Chromebookを再起動
-echo "Chromebookを再起動してください。"
-sudo reboot
