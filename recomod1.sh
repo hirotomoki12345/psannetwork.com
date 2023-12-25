62ffd3099026ba2683389385f46e9013147ece63
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-25 17:24:00 +0900

    Update recomod1.sh

--- recomod1.sh
+++ recomod1.sh
@@ -1,76 +1,66 @@
 #!/bin/bash
-
 set -e
 
-# ダウンロード先ディレクトリ
 DOWNLOAD_DIR="/home/chronos/user/Downloads"
-
-# エラーファイルのディレクトリとファイル名
 ERROR_DIR="$DOWNLOAD_DIR/flasherror"
 ERROR_LOG="$ERROR_DIR/error_log.txt"
+BACKUP_DIR="$DOWNLOAD_DIR"
 
-# バックアップ先ディレクトリ
-BACKUP_DIR="/home/chronos/user/Downloads"
+mkdir -p "$ERROR_DIR" "$BACKUP_DIR"
 
-# エラーファイルのディレクトリが存在しなければ作成
-mkdir -p "$ERROR_DIR"
-
-# バックアップ先ディレクトリが存在しなければ作成
-mkdir -p "$BACKUP_DIR"
-
-# エラー時の処理を定義
 function cleanup_and_exit {
-    local exit_code=$?
-    echo "エラー: $1"
-    
-    # エラーメッセージをファイルに保存
     echo "エラー: $1" >> "$ERROR_LOG"
-    
-    # ダウンロードしたファイルを削除
     rm -f "$DOWNLOAD_DIR/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin.zip"
-    
-    exit $exit_code
+    exit 1
 }
 
-# トラップを設定してエラーが発生した場合に cleanup_and_exit を呼ぶ
 trap 'cleanup_and_exit "スクリプトの実行中にエラーが発生しました."' ERR
 
-# ルートディレクトリのマウントを確認
+# ルートディレクトリがマウントされている場合、アンマウント
 if mount | grep " / " | grep -q "rw"; then
-    # ルートディレクトリが読み取り専用でない場合はアンマウント
     sudo umount / || cleanup_and_exit "ルートディレクトリのアンマウント中にエラーが発生しました。"
 fi
 
-# モードを引数から取得する
 mode="$1"
 
-# 選択されたモードに応じて処理を実行
 if [ "$mode" == "1" ]; then
-    # ダウンロードしたイメージをディスクに書き込み
-    curl -L "https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin.zip" -o "$DOWNLOAD_DIR/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin.zip" || cleanup_and_exit "イメージのダウンロード中にエラーが発生しました."
-
-    # Chromebookでは標準のunzipコマンドではなくbsdtarを使用する
-    bsdtar -xvf "$DOWNLOAD_DIR/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin.zip" -C "$DOWNLOAD_DIR" || cleanup_and_exit "イメージの展開中にエラーが発生しました."
-
-    # イメージをディスクに書き込み
-    sudo dd if="$DOWNLOAD_DIR/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin" of=/dev/mmcblk0 bs=4M conv=fsync || cleanup_and_exit "イメージの書き込み中にエラーが発生しました."
-
-    # 書き込みが成功したかを確認
-    echo "イメージの書き込みが成功しました。"
-
-    # Chromebookでは再起動はできないため、メッセージの表示のみ
-    echo "Chromebookを再起動してください。"
+    echo "Mode 1 開始"
+    
+    # リカバリイメージから必要な部分だけを取り出す
+    unzip -d "$DOWNLOAD_DIR/temp" "$DOWNLOAD_DIR/chromeos_15359.58.0_kukui_recovery_stable-channel_mp-v6.bin.zip" || cleanup_and_exit "イメージの展開中にエラーが発生しました."
+    sudo dd if="$DOWNLOAD_DIR/temp/sbin/chromeos-recovery" of="$DOWNLOAD_DIR/chromeos.img" bs=4M conv=fsync || cleanup_and_exit "イメージの取得中にエラーが発生しました."
+    rm -rf "$DOWNLOAD_DIR/temp"
+    echo "リカバリイメージ取得成功"
+    
+    # リカバリイメージを展開して必要な部分だけを取得
+    mkdir -p "$DOWNLOAD_DIR/recovery_temp"
+    sudo mount -o loop "$DOWNLOAD_DIR/chromeos.img" "$DOWNLOAD_DIR/recovery_temp"
+    sudo cp -a "$DOWNLOAD_DIR/recovery_temp"/* /  # 必要なディレクトリに変更する
+    sudo umount "$DOWNLOAD_DIR/recovery_temp"
+    
+    echo "イメージの書き込み成功"
+    echo "Chromebookを再起動してください."
     reboot
 
 elif [ "$mode" == "2" ]; then
-    # バックアップ先ディレクトリにバックアップイメージを作成
-    backup_file="$BACKUP_DIR/chromeos_backup_$(date +"%Y%m%d_%H%M%S").img"
-    sudo dd if=/dev/mmcblk0 of="$backup_file" bs=4M conv=fsync || cleanup_and_exit "バックアップの作成中にエラーが発生しました."
+    echo "Mode 2 開始"
     
-    # バックアップが成功したかを確認
-    echo "バックアップの作成が成功しました。"
+    remaining_space=$(df -h "$BACKUP_DIR" | awk 'NR==2 {print $4}')
+    echo "残りのディスク容量: $remaining_space"
     
+    # Ensure the backup directory is empty
+    sudo rm -rf "$BACKUP_DIR/current_os_backup"
+    sudo mkdir -p "$BACKUP_DIR/current_os_backup"
+    
+    # Exclude unnecessary directories during backup
+    exclude_dirs="--exclude='/proc/*' --exclude='/sys/*' --exclude='/run/*' --exclude='/dev/*' --exclude="$BACKUP_DIR/*""
+    
+    # Use dd to create a disk image excluding unnecessary directories
+    sudo dd if=/dev/mmcblk0 bs=4M conv=fsync status=progress | gzip > "$BACKUP_DIR/current_os_backup/chromeos_backup_$(date +"%Y%m%d_%H%M%S").img.gz" || cleanup_and_exit "OSのバックアップ中にエラーが発生しました."
+    
+    echo "OSのバックアップが成功しました."
+    echo "スクリプトがここまで実行されました."
+
 else
-    # 無効なモードが選択された場合のエラー処理
-    cleanup_and_exit "無効なモードが選択されました。"
+    cleanup_and_exit "無効なモードが選択されました."
 fi
