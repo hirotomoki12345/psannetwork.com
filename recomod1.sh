6961e950ba9457b391a37f48911586198e6a1eaf
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-26 18:01:58 +0900

    Update recomod1.sh

--- recomod1.sh
+++ recomod1.sh
@@ -1,4 +1,15 @@
-curl -LO https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.112.0_kukui_recovery_stable-channel_mp-v4.bin
-chromeos-install \
-  --image chromeos_image_latest.bin \
-  --device /dev/mmcblk0
+#!/bin/bash
+
+# ダウンロード先ディレクトリ
+download_dir="/home/chronos/user/Downloads"
+
+# イメージのダウンロード
+image_url="https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.112.0_kukui_recovery_stable-channel_mp-v4.bin"
+image_file="${download_dir}/chromeos_15117.112.0_kukui_recovery_stable-channel_mp-v4.bin"
+
+# イメージのダウンロード
+curl -L -o "${image_file}" "${image_url}"
+
+# インストール
+install_command="chromeos-install --dst /dev/mmcblk0 --image ${image_file}"
+${install_command} 2> "${download_dir}/install_error.txt"
