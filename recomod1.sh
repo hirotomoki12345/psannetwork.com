2af44ac8590c77cba747a8d52b861623fa0fa092
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-26 19:11:10 +0900

    Update recomod1.sh

--- recomod1.sh
+++ recomod1.sh
@@ -1,1 +1,7 @@
-chromeos-install --recovery --image-version 98.0.4758.109 --language ja --locale ja_JP
+# リカバリイメージをダウンロードする
+curl -L https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.112.0_kukui_recovery_stable-channel_mp-v4.bin.zip
+# リカバリイメージを解凍する
+unzip chromeos_15117.112.0_kukui_recovery_stable-channel_mp-v4.bin
+
+# 内部ディスクに書き込む
+dd if=chromeos_15117.112.0_kukui_recovery_stable-channel_mp-v4.bin -rw-image of=/dev/sda
