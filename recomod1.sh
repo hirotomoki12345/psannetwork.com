ec0c08c4ba4796de6eae957f0a6a73ce40396ac2
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-26 18:19:46 +0900

    Update recomod1.sh

--- recomod1.sh
+++ recomod1.sh
@@ -1,1 +1,36 @@
-sudo chromeos-install --skip_src_removable --dst /dev/sda
+#!/bin/bash
+
+# ブロックデバイスの一覧を表示
+lsblk
+
+# Chrome OSをインストールするデバイスを指定
+device="/dev/mmcblk0"  # 実際の環境に応じてデバイス名を変更してください
+
+# ダウンロード先ディレクトリ
+download_dir="/home/chronos/user/Downloads"
+
+# イメージのダウンロード
+image_url="https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.112.0_kukui_recovery_stable-channel_mp-v4.bin"
+image_file="${download_dir}/chromeos_15117.112.0_kukui_recovery_stable-channel_mp-v4.bin"
+
+# イメージのダウンロード
+curl -L -o "${image_file}" "${image_url}"
+
+# ダウンロードの成功確認
+if [ $? -ne 0 ]; then
+  echo "イメージのダウンロードに失敗しました。スクリプトを終了します。"
+  exit 1
+fi
+
+# デバッグ情報
+echo "インストールを開始します..."
+
+# Chrome OSをインストール
+sudo chromeos-install --skip_src_removable --dst "${device}"
+
+# インストールの成功確認
+if [ $? -eq 0 ]; then
+  echo "インストールが成功しました。"
+else
+  echo "インストールに失敗しました。エラーの詳細は上記の出力を確認してください。"
+fi
