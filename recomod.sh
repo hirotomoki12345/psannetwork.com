56615163c257fab133268b1c9c5181fc40b29311
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-30 16:20:39 +0900

    Update recomod.sh

--- recomod.sh
+++ recomod.sh
@@ -1,4 +1,2 @@
 
-はい、可能です。次のコマンドを実行します。
-
 chromeos-install --dst /dev/mmcblk0 --payload-image '/home/chronos/user/Downloads/recovery-image.bin' 2>&1 | tee /home/chronos/user/Downloads/chromeos-install.txt
