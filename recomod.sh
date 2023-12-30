722e558fde551592932d80d31afcecea35819bea
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-30 16:40:52 +0900

    Update recomod.sh

--- recomod.sh
+++ recomod.sh
@@ -1,1 +1,1 @@
-sudo chromeos-install --dst /dev/mmcblk0 /dev/sda 2>&1 | tee /home/chronos/user/Downloads/chromeos-install.
+sudo chromeos-install --dst /dev/mmcblk0 /dev/sda/image.bin 2>&1 | tee /home/chronos/user/Downloads/chromeos-install.txt
