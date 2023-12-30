a7fe177e13bd2c84284b52c33adab484c1397917
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-30 16:09:55 +0900

    Update recomod.sh

--- recomod.sh
+++ recomod.sh
@@ -1,5 +1,5 @@
 # 出力を変数に格納する
 output=$(chromeos-install -h)
 
-# 変数の内容をファイルに保存する
-echo "$output" > /home/chronos/user/Downloads/test.txt
+# 出力をファイルに保存する
+echo "$output" | grep -v "^$" > /home/chronos/user/Downloads/test.txt
