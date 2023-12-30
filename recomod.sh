d9881e7dbb536de2d8892f848dd15421ea5f7ff4
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-30 16:16:10 +0900

    Update recomod.sh

--- recomod.sh
+++ recomod.sh
@@ -1,1 +1,1 @@
-chromeos-install --dst /dev/mmcblk0 --payload-image /home/chronos/user/Downloads/recovery-image.bin
+chromeos-install --dst /dev/mmcblk0 --payload-image '/home/chronos/user/Downloads/recovery-image.bin'
