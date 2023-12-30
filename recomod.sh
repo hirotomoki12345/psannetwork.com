5097a86917275f265ba42ddd93616e9c7970ac1f
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-30 16:08:18 +0900

    Update recomod.sh

--- recomod.sh
+++ recomod.sh
@@ -1,26 +1,5 @@
-#!/bin/bash
+# 出力を変数に格納する
+output=$(chromeos-install -h)
 
-# コマンドラインオプションを解析する
-args=$(getopt -o rno -l recovery,no-os-check,image: -- "$@")
-
-# 解析結果を変数に格納する
-eval set -- "$args"
-
-# リカバリオプションが指定されているか確認する
-if [ -n "${1}" ]; then
-  # エラーメッセージを表示する
-  echo "エラー: コマンドラインオプションが正しくありません。"
-  echo "chromeos-install -h を実行して、コマンドのヘルプを表示してください。"
-  exit 1
-fi
-
-# リカバリイメージが指定されているか確認する
-if [ -z "${image}" ]; then
-  # エラーメッセージを表示する
-  echo "エラー: リカバリイメージが指定されていません。"
-  echo "chromeos-install -h を実行して、コマンドのヘルプを表示してください。"
-  exit 1
-fi
-
-# リカバリを実行する
-chromeos-install --recovery --no-os-check --image="${image}"
+# 変数の内容をファイルに保存する
+echo "$output" > /home/chronos/user/Downloads/test.txt
