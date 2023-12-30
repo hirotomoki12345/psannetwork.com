94d3c6b7136515962fe7c8770bdaab0beb0dad50
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-30 16:14:23 +0900

    Update recomod.sh

--- recomod.sh
+++ recomod.sh
@@ -1,5 +1,1 @@
-# 出力を変数に格納する
-output=$(chromeos-install -h)
-
-# 出力をファイルに保存する
-echo "$output" | grep -v "^$" > /home/chronos/user/Downloads/test.txt
+chromeos-install --dst /dev/mmcblk0 --payload-image /home/chronos/user/Downloads/recovery-image.bin
