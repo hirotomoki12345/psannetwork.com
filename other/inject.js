359272d6520aeb22f2a16e8e3c5cc7b6d6d9b710
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-31 17:05:17 +0900

    Update inject.js

--- other/inject.js
+++ other/inject.js
@@ -1,1 +1,25 @@
-javascript:(function(){var t=document.body;var e=document.createElement("style");e.textContent='body,html{height:100%;margin:0;}input[type="text"]{width:100%;height:100%;font-size:18px;padding:10px;box-sizing:border-box;}button{display:block;margin:10px auto;font-size:18px;}';t.appendChild(e);var n=document.createElement("input");n.type="text";n.placeholder="JavaScriptコードを入力してください";t.appendChild(n);var o=document.createElement("button");o.textContent="実行";o.addEventListener("click",function(){try{var e=n.value;eval(e)}catch(e){alert("エラー: "+e.message)}});t.appendChild(o)})();
+// inject.js
+
+(function() {
+  var body = document.body;
+  var style = document.createElement("style");
+  style.textContent = 'body,html{height:100%;margin:0;}input[type="text"]{width:100%;height:100%;font-size:18px;padding:10px;box-sizing:border-box;}button{display:block;margin:10px auto;font-size:18px;}';
+  body.appendChild(style);
+
+  var input = document.createElement("input");
+  input.type = "text";
+  input.placeholder = "JavaScriptコードを入力してください";
+  body.appendChild(input);
+
+  var button = document.createElement("button");
+  button.textContent = "実行";
+  button.addEventListener("click", function() {
+    try {
+      var code = input.value;
+      eval(code);
+    } catch (error) {
+      alert("エラー: " + error.message);
+    }
+  });
+  body.appendChild(button);
+})();
