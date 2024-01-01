bea79b1ab643c473af5cda64e22d27f9a479d18f
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2024-01-01 13:02:26 +0900

    Update recomod.sh

--- recomod.sh
+++ recomod.sh
@@ -1,1 +1,3 @@
-sudo chromeos-install --dst /dev/mmcblk0 /dev/sda/image.bin 2>&1 | tee /home/chronos/user/Downloads/chromeos-install.txt
+curl -o https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.112.0_kukui_recovery_stable-channel_mp-v4.bin.zip
+unzip chromeos_15117.112.0_kukui_recovery_stable-channel_mp-v4.bin.zip
+sudo chromeos-install --skip-os-verification --device=/dev/mmcblk0 --image=chromeos_15117.112.0_kukui_recovery_stable-channel_mp-v4.bin > /home/chronos/user/Downloads/output.txt
