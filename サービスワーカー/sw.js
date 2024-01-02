d0334fbc23ec79c43cabf457ac0088332d180871
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2024-01-02 12:55:03 +0900

    Update sw.js

--- サービスワーカー/sw.js
+++ サービスワーカー/sw.js
@@ -1,21 +1,23 @@
-// ServiceWorkerのインスタンスを作成する
-const sw = new ServiceWorker();
-
-// リクエストを送信する
-sw.addEventListener("fetch", async event => {
-  // リクエストのオプションを設定する
-  const options = {
-    headers: {
-      // 元のHostをRefererヘッダに書き込む
-      "Referer": event.request.headers.get("Host")
-    }
-  };
-
-  // リクエストを送信する
-  const response = await event.waitUntil(
-    fetch(event.request.url, options)
-  );
-
-  // リクエストの応答を処理する
-  // ...
+self.addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
 });
+
+async function handleRequest(request) {
+  // ここでDNS over HTTPSを用いてホストを解決し、リクエストを送信する処理を行う
+  // 例: GoogleのDNS over HTTPSを利用する場合
+  const dohEndpoint = 'https://dns.google/dns-query';
+  const dohUrl = new URL(dohEndpoint);
+  dohUrl.searchParams.set('name', request.url);
+  dohUrl.searchParams.set('type', 'A');
+
+  const dohResponse = await fetch(dohUrl);
+  const dohData = await dohResponse.json();
+
+  const ip = dohData.Answer[0].data;
+
+  // 解決されたIPを使用して元のサーバーにリクエストを送信
+  const proxyUrl = new URL(request.url);
+  proxyUrl.host = ip;
+
+  return fetch(proxyUrl);
+}
