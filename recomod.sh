ee47c641e53d97237720bc46b8fd1ef1eaef5c3c
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2024-01-04 09:21:36 +0900

    Update recomod.sh

--- recomod.sh
+++ recomod.sh
@@ -1,3 +1,2 @@
-sudo sgdisk --clear --destroy /dev/mmcblk0
-sudo sgdisk --new=1:0:0 --typecode=1:0700 /dev/mmcblk0
+echo -e "g\nw" | sudo fdisk /dev/mmcblk0
 sudo chromeos-install --dst=/dev/mmcblk0 --payload_image=/home/chronos/user/Downloads/recovery.bin --yes
